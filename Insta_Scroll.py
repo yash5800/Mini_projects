56b79aa1a90c3b294a12f7f4762dc2ccb4b45263
A.Yaswanth Kiran <113412043+yash5800@users.noreply.github.com>
2023-10-03 21:10:06 +0530

    Add files via upload

---
+++ Insta_Scroll.py
@@ -0,0 +1,7 @@
+import pyautogui #make sure to install pyautogui in your device using 'pip install pyautogui'
+import time
+import random
+Times = [5,10,15] #change time if you want to 'Note:it is seconds'
+while True:
+    time.sleep(random.choice(Times)) 
+    pyautogui.press('space')      
